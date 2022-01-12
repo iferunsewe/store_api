430da159d248d2844bff3f44d18d92812e33806f
Ife Runsewe <iferunsewe@gmail.com>
2022-01-12 17:38:01 +0100

    Validations added (and tested) to item model. Factory bot added to help with testing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  validates_presence_of :name, :code, :price
+  validates_uniqueness_of :code
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  it "has a valid factory" do
+    expect(build(:item)).to be_valid
+  end
+
+  it "is invalid without a name" do
+    expect(build(:item, name: nil)).to_not be_valid
+  end
+
+  it "is invalid without a code" do
+    expect(build(:item, code: nil)).to_not be_valid
+  end
+
+  it "is invalid without a price" do
+    expect(build(:item, price: nil)).to_not be_valid
+  end
+
+  it "is invalid with a duplicate code" do
+    create(:item, code: 'MUG')
+    expect(build(:item, code: 'MUG')).to_not be_valid
+  end
 end
