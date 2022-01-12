943b1404c8d732fab4fbab7a6d731635c4e0b5f2
Ife Runsewe <iferunsewe@gmail.com>
2022-01-12 17:39:02 +0100

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Item.create({code: 'MUG', name: 'Reedsy Mug', price: 600})
+Item.create({code: 'TSHIRT', name: 'Reedsy T-shirt', price: 1500})
+Item.create({code: 'HOODIE', name: 'Reedsy Hoodie', price: 2000})
