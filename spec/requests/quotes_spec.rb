1a142e960dd182ce8df329e29cc4aeebd783a6ec
Ife Runsewe <iferunsewe@gmail.com>
2022-01-13 19:32:13 +0100

    Tidying up

--- spec/requests/quotes_spec.rb
+++ spec/requests/quotes_spec.rb
@@ -3,15 +3,13 @@ require 'rails_helper'
 RSpec.describe '/quotes', type: :request do
   let(:quote) { create(:quote) }
 
-  let(:valid_attributes) { { item_codes: %w[ITEM-1 ITEM-2] } }
-
-  let(:invalid_attributes) { { item_codes: [] } }
-
   describe 'POST /create' do
     context 'with valid parameters' do
+      let(:valid_attributes) { { item_codes: %w[ITEM-1 ITEM-2] } }
+      
       before do
-        create(:item, code: 'ITEM-1', name: 'Item 1', price: 10.00)
-        create(:item, code: 'ITEM-2', name: 'Item 2', price: 20.00)
+        create(:item, code: 'ITEM-1', name: 'Item 1', price: 10)
+        create(:item, code: 'ITEM-2', name: 'Item 2', price: 20)
       end
 
       it 'creates a new Quote' do
@@ -26,11 +24,13 @@ RSpec.describe '/quotes', type: :request do
              params: { quote: valid_attributes }, as: :json
         expect(response).to have_http_status(:created)
         expect(response.content_type).to match(a_string_including('application/json'))
-        expect(JSON.parse(response.body)).to eq({"item_codes"=> %w[ITEM-1 ITEM-2], "total"=> 30.00})
+        expect(JSON.parse(response.body)).to eq({"item_codes"=> %w[ITEM-1 ITEM-2], "total"=> 30})
       end
     end
 
     context 'with invalid parameters' do
+      let(:invalid_attributes) { { item_codes: [] } }
+
       it 'does not create a new quote' do
         expect do
           post quotes_url,
