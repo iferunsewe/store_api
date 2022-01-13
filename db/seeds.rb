cac2b6e077413a206ca0fb24a472156ef1a68b39
Ife Runsewe <iferunsewe@gmail.com>
2022-01-13 19:26:37 +0100

    Updated the seeds with more examples for discounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,12 @@
 Item.create({ code: 'MUG', name: 'Reedsy Mug', price: 600 })
 Item.create({ code: 'TSHIRT', name: 'Reedsy T-shirt', price: 1500 })
 Item.create({ code: 'HOODIE', name: 'Reedsy Hoodie', price: 2000 })
+
+Discount.create({ discount_type: 'bundle', code: 'MUG', min_quantity: 2, discount_percentage: 0 })
+Discount.create({ discount_type: 'percentage', code: 'TSHIRT', min_quantity: 3, discount_percentage: 30 })
+
+Quote.create({ item_codes: ['MUG', 'TSHIRT', 'HOODIE'] })
+Quote.create({ item_codes: ['MUG', 'TSHIRT', 'MUG'] })
+Quote.create({ item_codes: ['MUG', 'TSHIRT', 'MUG', 'MUG'] })
+Quote.create({ item_codes: ['MUG', 'TSHIRT', 'MUG', 'MUG', 'MUG'] })
+Quote.create({ item_codes: ['MUG', 'TSHIRT', 'TSHIRT', 'TSHIRT', 'TSHIRT', 'MUG', 'HOODIE'] })
